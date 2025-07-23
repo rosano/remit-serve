4446c483f80bfecc5a576477064d6342a020975b
Rosano Coutinho <github.com@xeari.com>
2025-07-23 15:22:01 +0200

    change notice in main.js

--- cloudron-start.sh
+++ cloudron-start.sh
@@ -6,4 +6,6 @@ if [[ ! -f /app/data/.env ]]; then
 	cp .env-sample /app/data/.env
 fi
 
+sed -i '' -e 's/in \.env/in \/app\/data\/\.env/g' main.js
+
 exec /usr/local/bin/gosu cloudron:cloudron npm start
