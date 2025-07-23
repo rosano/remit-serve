935e22c1e332d5c8a658bb136a12ffbd83413eb2
Rosano Coutinho <github.com@xeari.com>
2025-07-23 15:18:41 +0200

    copy env-sample

--- cloudron-start.sh
+++ cloudron-start.sh
@@ -2,4 +2,8 @@
 
 # ensure that this file executable in the app repo: chmod +x start.sh.
 
+if [[ ! -f /app/data/.env ]]; then
+	cp .env-sample /app/data/.env
+fi
+
 exec /usr/local/bin/gosu cloudron:cloudron npm start
