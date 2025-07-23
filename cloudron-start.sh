8dbfa2ea4e35da4761d78a5c3a3487b02b4b04f6
Rosano Coutinho <github.com@xeari.com>
2025-07-23 15:34:31 +0200

    read env files in cloudron folder

--- cloudron-start.sh
+++ cloudron-start.sh
@@ -6,4 +6,8 @@ if [[ ! -f /app/data/.env ]]; then
 	cp .env-sample /app/data/.env
 fi
 
+set -o allexport
+source /app/data/.env
+set +o allexport
+
 exec /usr/local/bin/gosu cloudron:cloudron npm start
