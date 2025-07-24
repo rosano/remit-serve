f15f375f8db54d3a724397b537a608f40ffe73e4
Rosano Coutinho <github.com@xeari.com>
2025-07-24 09:55:38 +0200

    tidy

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const app = express()
 
 require('dotenv').config({ quiet: true })
 
-const port = process.env.PORT || 3000
-const base = process.env.REMIT_BASE
 
 app.use(async (req, res) => {
+	const base = process.env.REMIT_BASE
+	
 	if (!base || (typeof base === 'string' && !base.match(/^https?:\/\//))) {
 		return res.send('Please set REMIT_BASE to a URL in .env, then restart.')
 	}
@@ -18,6 +18,7 @@ app.use(async (req, res) => {
 	return res.send(await response.text())
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
 	console.info(`Listening on port ${ port }`)
 })
